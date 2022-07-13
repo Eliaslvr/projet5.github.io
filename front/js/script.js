7c3b5f840649d6c9a5349e8e70e5de0cadc2e97e
Eliaslvr <eliaslvr59@gmail.com>
2022-07-13 17:58:06 +0300

    Etape 3 validé

--- front/js/script.js
+++ front/js/script.js
@@ -1,28 +1,27 @@
-/*fetch("http://localhost:3000/api/products")
-.then(function(res) {
-    if(res.ok) {
-        return res.json();
-    }
-})
-.then(function(value) {
-    console.log(value)
-})
-.catch(function(err) {
-    document.querySelector("body").innerHTML = "<body>erreur 404</body>"
-    console.log("erreur " + err);
-})
-/*
-fetch("http://localhost:3000/api/products")
-    .then(result => result.json())
-    .then(data => console.log(data))
-    .then(err => console.log("Erreur 404"))*/
+fetch("http://localhost:3000/api/products/") 
+    .then(function (reponse) {
+        return reponse.json()
+    })
+    .then(function (afficheKanap) {
+        console.log(afficheKanap);
+    Kanap(afficheKanap);
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
 
-const getUser = async function() {
-    let response = await fetch("/api/products")
-    if (response.ok) {
-        let data = await response.json()
-        console.log(data)
-    } else {
-        console.error("Retour du serveur : ", response.status)
+
+function Kanap(affiche) {
+    let articles = document.getElementById("items");
+    for (let article of affiche) {
+        articles.innerHTML += `<a href="./product.html?id=${article._id}">
+        <article>
+          <img src="${article.imageUrl}" alt="${article.altTxt}">
+          <h3 class="productName">${article.name}</h3>
+          <p class="productDescription">${article.description}</p>
+        </article>
+      </a> `;
     }
 }
+
+
