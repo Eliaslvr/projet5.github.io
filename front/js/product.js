065bb55c586e967ce977a41d36fad48765326e8b
Eliaslvr <eliaslvr59@gmail.com>
2023-02-01 00:13:10 +0300

    Modification

--- front/js/product.js
+++ front/js/product.js
@@ -27,41 +27,82 @@ function getArticle() {
 //function addToCart(product){
   const button = document.querySelector("#addToCart")
   button.addEventListener("click", (e) => {
+    e.preventDefault();//annulation de tout les comportements par defaut
     let id = articlId
     let couleurs = document.querySelector('#colors').value
-    let quantité = document.querySelector('#quantity').value
+    let quantite = document.querySelector('#quantity').value
     let tableData = [];
-    let getLocalStorageAffiche = JSON.parse(localStorage.getItem('obj'))
-    quantité = Number(quantité.value)
-    //quantité = incrementQuantity(quantité);
+    let getLocalStorageData = localStorage.getItem('obj')
 
-    if (quantité < 1 || quantité > 99 || couleurs == null || couleurs == "" || couleurs == '--SVP, choisissez une couleur --') {
+    if (quantite < 1 || quantite > 99 || couleurs == null || couleurs == "" || couleurs == '--SVP, choisissez une couleur --') {
       alert("Séléctionner une couleur ainsi qu'un article compris entre 1 et 99")
     } else {
-      let objKanap = {
-        id,
-        //name: product.name,
-        //img: product.imageUrl,
-        couleurs,
-        quantité,
-      }
-      if (getLocalStorageAffiche) {
-        let item = getLocalStorageAffiche.find(
-          (item) => item.id == objKanap.id && item.couleurs == objKanap.couleurs
-        )
-        if (item) {
-          const newQuantity = item.quantité + objKanap.quantité;
-          //console.log(newQuantity)
-          item.quantité = newQuantity;
-
-          localStorage.setItem("obj", JSON.stringify(getLocalStorageAffiche));
+        if(!getLocalStorageData) {
+          let objKanap = {
+            id,
+            couleurs,
+            quantite,
+          }
+          tableData.push(objKanap);//Ajoute element dans le tableau
+          localStorage.setItem("obj", JSON.stringify(tableData));//Pouvoir ajouter l'objet au localStorage
         } else {
-          getLocalStorageAffiche.push(objKanap);
-          localStorage.setItem("obj", JSON.stringify(tableData))
+          tableData = JSON.parse(localStorage.getItem('obj'))//Pouvoir recuprer l'objet dans le localStorage
+          tableData.forEach(element => {
+            if (element.id == id && element.couleurs == couleurs) {
+              element.quantite = quantite
+              localStorage.setItem("obj", JSON.stringify(tableData));
+
+            } else {
+              console.log(id, couleurs, quantite);
+            }
+          });
         }
 
-    }}})
+    }})
 
+
+
+
+
+    /*tableData = JSON.parse(localStorage.getItem('obj'))//Pouvoir recuprer l'objet dans le localStorage
+          tableData.forEach(element => {
+            if (element.id == id && element.couleurs == couleurs) {
+              element.quantite = quantite
+              localStorage.setItem("obj", JSON.stringify(tableData));
+
+            } else {
+              objKanap = {
+                id,
+                couleurs,
+                quantite,
+              }
+              tableData.push(objKanap); 
+              localStorage.setItem("obj", JSON.stringify(tableData));    
+              return false;
+            }
+          })*/
+
+    /*let objKanap = {
+      id,
+      //name: product.name,
+      //img: product.imageUrl,
+      couleurs,
+      quantité,
+    }
+    if (getLocalStorageAffiche) {
+      let item = getLocalStorageAffiche.find(
+        (item) => item.id == objKanap.id && item.couleurs == objKanap.couleurs
+      )
+      if (item) {
+        const newQuantity = item.quantité + objKanap.quantité;
+        //console.log(newQuantity)
+        item.quantité = newQuantity;
+
+        localStorage.setItem("obj", JSON.stringify(getLocalStorageAffiche));
+      } else {
+        getLocalStorageAffiche.push(objKanap);
+        localStorage.setItem("obj", JSON.stringify(tableData))
+      }*/
 
 
 
