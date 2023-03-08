72182cc1e7080644c4a7994a5b60c93c9e3ad9b9
Eliaslvr <eliaslvr59@gmail.com>
2023-03-08 19:21:25 +0300

    Ajout cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -13,7 +13,7 @@ if (!getLocalStorageCart) {
     `
   afficheElement.innerHTML = panierVide
 } else {
-  getLocalStorageCart.forEach((element, index) => { // index représente la position de l'élément dans le tableau
+  getLocalStorageCart.forEach((element) => { 
     fetch(`http://localhost:3000/api/products/${element.id}`)
       .then(data => data.json())
       .then(jsonAffiche => {
@@ -39,43 +39,166 @@ if (!getLocalStorageCart) {
                                           </div>
                                         </div>
                                       </article> `
+                              //////////////SUPPRESSION///////////////
+          Array.from(document.getElementsByClassName("deleteItem")).forEach(element => {//convertir tableau document.getElement.....
+            element.addEventListener("click", (e) => {
+              const id = element.getAttribute('dataId');
+              const color = element.getAttribute('dataColor');
+              const local = localStorage.getItem("obj");
+              const products = JSON.parse(local);
+              console.log(id);
+              console.log(color);
+              const produitFind = products.findIndex(element => element.id === id && element.couleur === color); 
+              console.log(produitFind);
+              products.splice(produitFind, 1);
+              localStorage.setItem("obj", JSON.stringify(products));
+              location.reload();
+            })
+          });
+
+                               //////////////AJOUT LOCAL STORAGE//////////////
+            const quantiy = Array.from(document.getElementsByClassName("itemQuantity"));
+            quantiy.forEach((element, i) => {
+              element.addEventListener("input", (e) => { //input se declanche quand la valeur a été modifier
+                const id = element.getAttribute('dataId');
+                const color = element.getAttribute('dataColor');
+                const local = localStorage.getItem("obj");
+                const products = JSON.parse(local);
+                const produitFind = products.find(element => element.id === id && element.couleur === color); 
+                console.log(produitFind);
+                produitFind.quantite = e.target.value;//target renvoie l'élément sur lequel l'événement s'est produit
+                localStorage.setItem("obj", JSON.stringify(products));
+              })
+            });
+                          ////////////QUANTITE FINAL///////////
+            let totalQuantity = [];
+
+              for (let i = 0; i < getLocalStorageCart.length; i++) {
+                let quantiyAccum = getLocalStorageCart[i].quantite;
+                parseQuantite = parseInt(quantiyAccum)
+                totalQuantity.push(parseQuantite);
+
+                const quantityFinal = totalQuantity.reduce(
+                  (accumulator, currentValue) => accumulator + currentValue
+                );
+
+            
+
+                //const getPrice = document.getElementById('totalPrice');
+                const getQuantity = document.getElementById('totalQuantity');
+    
+                //getPrice.textContent = "1"
+                getQuantity.textContent = quantityFinal;
+              }
+                         //////////////PRIX FINAL/////////////////////
+            let totalPrice = [];
+
+            for (let i in jsonAffiche) {
+              //console.log(jsonAffiche[i]);
+              let priceAccum = jsonAffiche.price;
+              console.log(priceAccum);
+              totalPrice.push(priceAccum);
+              console.log(totalPrice);
+
+              // const priceFinal = totalPrice.reduce(
+              //   (accumulator, currentValue) => accumulator + currentValue
+              // );
+              // console.log(priceFinal);
+            }
+            // let totalPrice = [];
+            // const abc = jsonAffiche;
+              
+            //   for (let i = 0; i < jsonAffiche.length; i++) {
+            //     // let priceAccum = jsonAffiche[i].price;
+            //     // console.log(jsonAffiche);
+            //     // console.log(priceAccum);
+            //     // parsePrice = parseInt(PriceAccum)
+            //     // totalPrice.push(parsePrice);
+
+            //     // const priceFinal = totalPrice.reduce(
+            //     //   (accumulator, currentValue) => accumulator + currentValue
+            //     // );
+
+            
+
+            //     // const getPrice = document.getElementById('totalPrice');
+            //     // const getQuantity = document.getElementById('totalQuantity');
+    
+            //     // getPrice.textContent = "1"
+            //     // getQuantity.textContent = quantityFinal;
+            //   }
       })
-      .then(() => {
-        Array.from(document.getElementsByClassName("deleteItem")).forEach(element => {//convertir tableau document.getElement.....
-          element.addEventListener("click", (e) => {
-            const id = element.getAttribute('dataId');
-            const color = element.getAttribute('dataColor');
-            const local = localStorage.getItem("obj");
-            const products = JSON.parse(local);
-            console.log(id);
-            console.log(color);
-            const produitFind = products.findIndex(element => element.id === id && element.couleur === color); 
-            console.log(produitFind);
-            products.splice(produitFind, 1);
-            localStorage.setItem("obj", JSON.stringify(products));
-            location.reload();
-          })
-        });
-      })
-    .then(() => {
-      const quantiy = Array.from(document.getElementsByClassName("itemQuantity"));
-      quantiy.forEach((element, i) => {
-        element.addEventListener("input", (e) => { //input se declanche quand la valeur a été modifier
-          const id = element.getAttribute('dataId');
-          const color = element.getAttribute('dataColor');
-          const local = localStorage.getItem("obj");
-          const products = JSON.parse(local);
-          const produitFind = products.find(element => element.id === id && element.couleur === color); 
-          console.log(produitFind);
-          produitFind.quantite = e.target.value;//target renvoie l'élément sur lequel l'événement s'est produit
-          localStorage.setItem("obj", JSON.stringify(products));
-        })
-        
-      });
-    })
   }
   )
+  // const getPrice = document.getElementById('totalPrice');
+  // const getQuantity = document.getElementById('totalQuantity');
+  // console.log(getLocalStorageCart);
+  // console.log(getPrice);
+  // console.log(getQuantity);
+
+  //  let totalQuantity = [];
+
+  //  for (let i = 0; i < getLocalStorageCart.length; i++) {
+  //   let totalQuantityPanier = getLocalStorageCart[i].quantite;
+
+  //   totalQuantity.push(totalQuantityPanier)
+  //   console.log(totalQuantity);
+
+  //   const calcul = (accumulator, currentValue) => accumulator + currentValue;
+  //   const quantityFinal = totalQuantity.reduce(calcul);
+
+  //   console.log(quantityFinal);
+  //  }
+  /*let totalQuantity = [];
+
+  for (let i = 0; i < getLocalStorageCart.length; i++) {
+    let quantiyAccum = getLocalStorageCart[i].quantite;
+    totalQuantity.push(quantiyAccum);
+    console.log(totalQuantity);
+
+    // const quantityFinal = totalQuantity.reduce(
+    //   (accumulator, currentValue) => accumulator + currentValue
+    // );
+    // console.log(quantityFinal);
+  }
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  console.log(reducer);
+  console.log(totalQuantity.reduce(reducer));*/
+
+   
+   
+    // getLocalStorageCart.forEach(element => {
+    //   console.log(element.quantite)
+    //   let totalQuant = [element.quantite]
+    //   let totalQuantity = 0;
+    //   for (let i = 0; i < totalQuant.length; i++) {
+    //     totalQuantity += totalQuant[i];
+    //   }
+    //   console.log(totalQuantity);
+
+    // });
+
+  //getPrice.innerHTML = `<p>Total (<span id="totalQuantity"> ${element.quantite} </span> articles) : <span id="totalPrice"> ${element.quantite * jsonAffiche.price} </span> €</p>`
+  // getPrice.textContent = "1"
+  // getQuantity.textContent = "1"
+  
 }
+// let totalQuantity = [];
+
+//   for (let i = 0; i < getLocalStorageCart.length; i++) {
+//     let quantiyAccum = getLocalStorageCart[i].quantite;
+//     parseQuantite = parseInt(quantiyAccum)
+//     //console.log(quantiyAccum);
+//     totalQuantity.push(parseQuantite);
+//     console.log(totalQuantity);
+//     console.log(parseQuantite);
+
+//     const quantityFinal = totalQuantity.reduce(
+//       (accumulator, currentValue) => accumulator + currentValue
+//     );
+//     console.log(quantityFinal);
+//   }
+
 
 
 // function validation()
