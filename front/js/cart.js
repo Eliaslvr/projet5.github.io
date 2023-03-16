c3f007504df28a07fa485ec6d138fa33b15d8681
Eliaslvr <eliaslvr59@gmail.com>
2023-03-16 13:51:54 +0300

    debut formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -62,7 +62,7 @@ if (!getLocalStorageCart) {
                 const local = localStorage.getItem("obj");
                 const products = JSON.parse(local);
                 const produitFind = products.find(element => element.id === id && element.couleur === color); 
-                if (e.target.value >= 1 && e.target.value<= 100){
+                if (e.target.value >= 1 && e.target.value <= 100){
                   produitFind.quantite = e.target.value;//target renvoie l'élément sur lequel l'événement s'est produit 
                 } else {
                   alert("Saisir une quantité compris entre 1 et 100")
@@ -96,6 +96,156 @@ function getTotalPrice() {
 }
 
 
+let form = document.querySelector(".cart__order__form");
+
+ /////PRENOM/////
+form.firstName.addEventListener('input', function() {
+  let RegExpFirstName = new RegExp('^[A-Z][A-Za-z\é\è\ê\-]+$', 'g');
+
+  let prenom = form.firstName.value;
+  console.log(prenom);
+
+  if(RegExpFirstName.test(prenom)) {
+    document.getElementById('firstNameErrorMsg').innerHTML = "Prénom Validé";
+    //firstName.style.background = "green";
+  } else if (prenom == null){
+    document.getElementById('firstNameErrorMsg').innerHTML = "";
+  } else if (RegExpFirstName.test(prenom) == false) {
+    document.getElementById('firstNameErrorMsg').innerHTML = "Prénom Non Validé";
+  }
+});
+
+form.lastName.addEventListener('input', function() {
+  // let RegExpLastName = new RegExp('^[A-Z][A-Za-z\é\è\ê\-]+$', 'g');
+  let RegExpLastName = new RegExp('^[a-zA-Z ]+$', 'g');
+
+  let nom = form.lastName.value;
+
+  if(RegExpLastName.test(nom)) {
+    document.getElementById('lastNameErrorMsg').innerHTML = "Nom Validé";
+  } else {
+    document.getElementById('lastNameErrorMsg').innerHTML = "Nom Non Validé"
+  }
+});
+
+form.address.addEventListener('input', function() {
+  // let RegExpAddress = new RegExp('/^[A-Za-z0-9]{5,50}$/', 'g');
+  let RegExpAddress = new RegExp('[A-Za-z0-9$]$');
+
+  let adresse = form.address.value;
+
+  if(RegExpAddress.test(adresse)) {
+    document.getElementById('addressErrorMsg').innerHTML = "Adresse Validé";
+  } else {
+    document.getElementById('addressErrorMsg').innerHTML = "Adresse Non Validé"
+  }
+});
+
+form.city.addEventListener('input', function() {
+  // let RegExpCity = new RegExp('/^[A-Za-z0-9]{5,50}$/', 'g');
+  let RegExpCity = new RegExp(`[A-Za-z]`, 'g');
+
+  let city = form.city.value;
+
+  if(RegExpCity.test(city)) {
+    document.getElementById('cityErrorMsg').innerHTML = "Ville Validé";
+  } else {
+    document.getElementById('cityErrorMsg').innerHTML = "Ville Non Validé"
+  }
+});
+
+form.email.addEventListener('input', function() {
+  let RegExpEmail = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+
+  let email = form.email.value;
+
+  if(RegExpEmail.test(email)) {
+    document.getElementById('emailErrorMsg').innerHTML = "Email Validé";
+    let emailcolor = document.getElementById('email')
+    //emailcolor.removeAttr("style")
+    //emailcolor.style.background = "green";
+  } else {
+    document.getElementById('emailErrorMsg').innerHTML = "Email Non Validé"
+    //emailcolor.removeAttr("style")
+    //emailcolor.style.background = "red";
+  }
+});
+
+// form.lastName.addEventListener('input', function() {
+//   validEmail(this);
+// });
+
+// const validLastName = function(inputEmail) {
+//   //Creation de la reg pour validation email
+//   let emailRegExp = new RegExp(
+//     '^[A-Z][A-Za-z\é\è\ê\-]+$', 'g'
+//     );
+
+//     //Recuperation de la base SMALL
+//     let small = inputEmail.nextElementSibling; //Attrappe l'element qui suit
+
+//     //On test l'expression reguliere
+//     if(emailRegExp.test(inputEmail.value) == true){
+//       small.innerHTML = "Adresse Valide";
+//       // small.classList.remove('text-danger');//remove signifie retiré cette classe si elle est présente
+//       // small.classList.add('text-success');
+//     } else {
+//       small.innerHTML = "Adresse Non Valide";
+//       //document.getElementById('email').style.background = "red"
+//       // small.classList.remove('text-success');
+//       // small.classList.add('text-danger');
+//     }
+// };
+
+// form.email.addEventListener('change', function() {
+//   validEmail(this);
+// });
+
+// const validEmail = function(inputEmail) {
+//   //Creation de la reg pour validation email
+//   let emailRegExp = new RegExp(
+//     '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g'
+//     );
+
+//     //Recuperation de la base SMALL
+//     let small = inputEmail.nextElementSibling; //Attrappe l'element qui suit
+
+//     //On test l'expression reguliere
+//     if(emailRegExp.test(inputEmail.value) == true){
+//       small.innerHTML = "Adresse Valide";
+//       // small.classList.remove('text-danger');//remove signifie retiré cette classe si elle est présente
+//       // small.classList.add('text-success');
+//     } else {
+//       small.innerHTML = "Adresse Non Valide";
+//       //document.getElementById('email').style.background = "red"
+//       // small.classList.remove('text-success');
+//       // small.classList.add('text-danger');
+//     }
+// };
+
+
+
+// let cartOrder = document.querySelector('.cart__order__form');
+
+// cartOrder.addEventListener('input', function(e) {
+
+//   let name = document.getElementById('lastName');
+//   let regex = /^[a-zA-Z\s]+$/;
+
+//   if (name.value == "") {
+//     let error = document.getElementById('firstNameErrorMsg');
+//     error.innerHTML = "Le champ est requis";
+//     error.style.color = "red";
+//     name.style.background = "red";
+//     e.preventDefault();
+//   } else if (regex.test(name.value) == false) {
+//     let error = document.getElementById('firstNameErrorMsg');
+//     error.innerHTML = "Le champ doit comporter des lettres uniquement";
+//     error.style.color = "red";
+//     name.style.background = "red";
+//     e.preventDefault();
+//   }
+// })
 
 // function validation()
 // {
